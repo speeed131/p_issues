05d51991072c6f01b19d6b85c82e3e873ef9519f
Daiki Hirose <speeed131@gmail.com>
2025-08-16 19:37:55 +0900

    :art: Square のストーリーブック

---
+++ test_issues/storybook/src/stories/Square.stories.js
@@ -0,0 +1,41 @@
+
+import { fn } from 'storybook/test'
+import { Square } from '../Square'
+
+const meta = {
+  title: 'Atoms/Square',
+  component: Square,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    value: {
+      control: { type: 'select' },
+      options: ['X', 'O', null],
+      description: 'マスの表示値',
+    },
+    onSquareClick: {
+      action: 'onSquareClick',
+      description: 'クリック時のコールバック',
+    },
+  },
+  args: {
+    value: null,
+    onSquareClick: fn(),
+  },
+}
+
+export default meta
+
+export const Empty = {
+  args: { value: null },
+}
+
+export const X = {
+  args: { value: 'X' },
+}
+
+export const O = {
+  args: { value: 'O' },
+}
